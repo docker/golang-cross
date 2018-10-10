246d9b30aab6af96f97d2bbe957099bc1e963dd6
Kir Kolyshkin <kolyshkin@gmail.com>
2018-10-10 14:01:43 -0700

    osx-cross.sh: add sha256 checksum validation
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- osx-cross.sh
+++ osx-cross.sh
@@ -14,6 +14,7 @@ time apt-get install -y -q --no-install-recommends $PKG_DEPS
 # NOTE: when changing version here, make sure to
 # also change OSX_CODENAME below to match
 OSX_SDK=MacOSX10.10.sdk
+SDK_SUM=631b4144c6bf75bf7a4d480d685a9b5bda10ee8d03dbf0db829391e2ef858789
 
 OSX_CROSS_COMMIT=a9317c18a3a457ca0a657f08cc4d0d43c6cf8953
 OSXCROSS_PATH="/osxcross"
@@ -29,6 +30,9 @@ git checkout -q $OSX_CROSS_COMMIT
 echo "Downloading OSX SDK"
 time curl -sSL https://s3.dockerproject.org/darwin/v2/${OSX_SDK}.tar.xz \
     -o "${OSXCROSS_PATH}/tarballs/${OSX_SDK}.tar.xz"
+
+echo "$SDK_SUM  ${OSXCROSS_PATH}/tarballs/${OSX_SDK}.tar.xz" \
+	| sha256sum -c -
 
 echo "Building osxcross"
 # Go 1.11 requires OSX >= 10.10
